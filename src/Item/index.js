bb9b39adc00c3027ae5d941e359efbf3fc7d4b1c
Becca Manning <rebm01@hotmail.co.uk>
2020-03-19 01:53:51 +0000

    Add Github pages & update formating

--- src/Item/index.js
+++ src/Item/index.js
@@ -5,36 +5,36 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import './Item.css';
 
 const Item = ({ id, img, title, size, price, seller, sold, brand, liked, toggleLike }) => (
-  <div className="item">
-    <div className="item-img-container">
-      <img className="item-img" alt={title} src={img} />
-      {sold && <div className="sold">SOLD</div>}
-      <button className={`like-button ${liked ? 'liked' : ''}`} onClick={() => toggleLike(id, title)}>
-        <FontAwesomeIcon className="heart" icon={faHeart} />
-      </button>
+    <div className="item">
+        <div className="item-img-container">
+        <img className="item-img" alt={title} src={img} />
+        {sold && <div className="sold">SOLD</div>}
+        <button className={`like-button ${liked ? 'liked' : ''}`} onClick={() => toggleLike(id, title)}>
+            <FontAwesomeIcon className="heart" icon={faHeart} />
+        </button>
+        </div>
+        <p><b>{seller}</b> {title}</p>
+        <p>{brand && <span>Brand: <b>{brand}</b></span>} {size && <span>Size: <b>{size}</b></span>}</p>
+        <p>Price <b>{`£${price}`}</b></p>
     </div>
-    <p><b>{seller}</b> {title}</p>
-    <p>{brand && <span>Brand: <b>{brand}</b></span>} {size && <span>Size: <b>{size}</b></span>}</p>
-    <p>Price <b>{`£${price}`}</b></p>
-  </div>
 );
 
 Item.propTypes = {
-  id: PropTypes.number.isRequired,
-  img: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  seller: PropTypes.string.isRequired,
-  sold: PropTypes.bool.isRequired,
-  size: PropTypes.string,
-  brand: PropTypes.string,
-  liked: PropTypes.bool.isRequired,
-  toggleLike: PropTypes.func.isRequired
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    seller: PropTypes.string.isRequired,
+    sold: PropTypes.bool.isRequired,
+    size: PropTypes.string,
+    brand: PropTypes.string,
+    liked: PropTypes.bool.isRequired,
+    toggleLike: PropTypes.func.isRequired
 }
 
 Item.defaultProps = {
-  size: null,
-  brand: null
+    size: null,
+    brand: null
 }
 
 export default Item;
