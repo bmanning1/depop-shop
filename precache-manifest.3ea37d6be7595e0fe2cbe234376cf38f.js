290c3ddfe31151bfc378cdeef63073f0ff72910f
Becca Manning <rebm01@hotmail.co.uk>
2020-03-19 01:45:10 +0000

    Updates

---
+++ precache-manifest.3ea37d6be7595e0fe2cbe234376cf38f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764bd90412a419e69e7cb45175d36239",
+    "url": "/depop-shop/index.html"
+  },
+  {
+    "revision": "92a1079984ac30ef8180",
+    "url": "/depop-shop/static/css/main.05299df9.chunk.css"
+  },
+  {
+    "revision": "dbf41dca8dd4b2a85035",
+    "url": "/depop-shop/static/js/2.32f86080.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/depop-shop/static/js/2.32f86080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a1079984ac30ef8180",
+    "url": "/depop-shop/static/js/main.64343dd8.chunk.js"
+  },
+  {
+    "revision": "e2aa04c2256b2075490d",
+    "url": "/depop-shop/static/js/runtime-main.4ec2ef19.js"
+  },
+  {
+    "revision": "d323bd3c33f4e597c0eff3bebe38fb5b",
+    "url": "/depop-shop/static/media/depop_logo.d323bd3c.png"
+  }
+]);
